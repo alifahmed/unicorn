b1b304d382ffa0793fdeed75558ae7bda6d55cda
Alif Ahmed <alifahmed@virginia.edu>
2020-06-15 03:22:02 -0400

    Added few in fuzz_all

--- tests/fuzz/fuzz_all.c
+++ tests/fuzz/fuzz_all.c
@@ -76,7 +76,29 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 
     uc_close(uc);
 
+	err = uc_open(UC_ARCH_MIPS, UC_MODE_MIPS32 + UC_MODE_LITTLE_ENDIAN, &uc);
+    if (err != UC_ERR_OK) {
+        printf("Failed on uc_open() with error returned: %u\n", err);
+        abort();
+    }
 
+    // map 4MB memory for this emulation
+    uc_mem_map(uc, ADDRESS, 4 * 1024 * 1024, UC_PROT_ALL);
+
+    // write machine code to be emulated to memory
+    if (uc_mem_write(uc, ADDRESS, Data, Size)) {
+        printf("Failed to write emulation code to memory, quit!\n");
+        abort();
+    }
+
+    // emulate code in infinite time & 4096 instructions
+    // avoid timeouts with infinite loops
+    err=uc_emu_start(uc, ADDRESS, ADDRESS + Size, 0, 0x1000);
+    if (err) {
+        fprintf(outfile, "Failed on uc_emu_start() with error returned %u: %s\n", err, uc_strerror(err));
+    }
+
+    uc_close(uc);
 
     return 0;
 }
